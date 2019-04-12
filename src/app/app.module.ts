28e486ae8eccdb2e9495109a0ee0f5566414c8cf
Malakatron <manelbermudez@gmail.com>
2019-04-12 19:54:16 +0200

    Añadida seccion heroes, y provider mediante servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+//Rutas
+import { APP_ROUTING } from './app.routes';
+
+//Servicios
+import { HeroesService } from './services/heroes.service';
+
+//Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -19,9 +25,11 @@ import { HeroesComponent } from './components/heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
